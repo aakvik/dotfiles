47b1e1cdb3ad110b642195b06e6d299f9e3ed89b
Aleksander Aakvik <a@uw.no>
2018-05-25 15:22:42 +0200

    fixed blabla

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -507,61 +507,44 @@ root.keys(globalkeys)
 
 -- {{{ Rules
 -- Rules to apply to new clients (through the "manage" signal).
-if screen.count() > 1 then
-    awful.rules.rules = {
-        -- All clients will match this rule.
-        { rule = { },
-          properties = { border_width = beautiful.border_width,
-                         border_color = beautiful.border_normal,
-                         focus = awful.client.focus.filter,
-                         raise = true,
-                         keys = clientkeys,
-                         buttons = clientbuttons,
-                         screen = awful.screen.preferred,
-                         placement = awful.placement.no_overlap+awful.placement.no_offscreen,
-                         size_hints_honor = false
-         }
-        },
+awful.rules.rules = {
+    -- All clients will match this rule.
+    { rule = { },
+      properties = { border_width = beautiful.border_width,
+                     border_color = beautiful.border_normal,
+                     focus = awful.client.focus.filter,
+                     raise = true,
+                     keys = clientkeys,
+                     buttons = clientbuttons,
+                     screen = awful.screen.preferred,
+                     placement = awful.placement.no_overlap+awful.placement.no_offscreen,
+                     size_hints_honor = false
+     }
+    },
 
-        -- Titlebars
-        { rule_any = { type = { "dialog", "normal" } },
-            properties = { titlebars_enabled = false } },
+    -- Titlebars
+    { rule_any = { type = { "dialog", "normal" } },
+        properties = { titlebars_enabled = false } },
 
-        -- Force some applications to start in spesific tags / screens
-
-        { rule = { class = "Slack" },
-            properties = { screen = 2, tag = awful.util.tagnames[3] } },
-
-        { rule = { class = "discord" },
-            properties = { screen = 2, tag = awful.util.tagnames[3] } },
-
-        { rule = { class = "Enpass" },
-            properties = { screen = 4, tag = awful.util.tagnames[9] } },
-
-        { rule = { class = "Kodi" },
-            properties = { screen = 4 } },
-
-    }
-else
-    awful.rules.rules = {
-        -- All clients will match this rule.
-        { rule = { },
-          properties = { border_width = beautiful.border_width,
-                         border_color = beautiful.border_normal,
-                         focus = awful.client.focus.filter,
-                         raise = true,
-                         keys = clientkeys,
-                         buttons = clientbuttons,
-                         screen = awful.screen.preferred,
-                         placement = awful.placement.no_overlap+awful.placement.no_offscreen,
-                         size_hints_honor = false
-         }
-        },
-
-        -- Titlebars
-        { rule_any = { type = { "dialog", "normal" } },
-            properties = { titlebars_enabled = false } },
+    -- Force some applications to start in spesific tags / screens
 }
+
+if screen.count() > 1 then
+        for _,rule in ipairs{
+            { rule = { class = "Slack" },
+                properties = { screen = 2, tag = awful.util.tagnames[3] } },
+
+            { rule = { class = "discord" },
+                properties = { screen = 2, tag = awful.util.tagnames[3] } },
+
+            { rule = { class = "Enpass" },
+                properties = { screen = 4, tag = awful.util.tagnames[9] } },
+
+            { rule = { class = "Kodi" },
+                properties = { screen = 4 } }
+        } do
+            table.insert(awful.rules.rules, rule)
+        end
 end
 
 -- {{{ Signals
