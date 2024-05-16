0a08631d3df40c0458865ea9019dc097538f8007
Aleksander Aakvik <aleksander@aakvik.net>
2024-05-16 16:13:35 +0200

    cleaned up and moved tmux.conf disabled setxkbmap

--- .config/awesome/autorun.sh
+++ .config/awesome/autorun.sh
@@ -7,8 +7,8 @@ function run {
   fi
 }
 
-setxkbmap -layout "no"
-setxkbmap -option caps:none
+#setxkbmap -layout "no"
+#setxkbmap -option caps:none
 run xscreensaver -nosplash &
 run nm-applet &
 #run compton --backend glx --paint-on-overlay --vsync opengl-swc --no-fading-openclose &
