ce28e5309ea3d27ce744192ed06d4ccdef79860f
Aleksander Aakvik <aleksander@aakvik.net>
2024-05-16 19:24:07 +0200

    removed picom for native tearfree

--- .config/awesome/autorun.sh
+++ .config/awesome/autorun.sh
@@ -1,15 +1,10 @@
 #!/usr/bin/env bash
 
 function run {
-  if ! pgrep $1 ;
-  then
-    $@&
-  fi
+	if ! pgrep $1; then
+		$@ &
+	fi
 }
 
-#setxkbmap -layout "no"
-#setxkbmap -option caps:none
 run xscreensaver -nosplash &
 run nm-applet &
-#run compton --backend glx --paint-on-overlay --vsync opengl-swc --no-fading-openclose &
-run picom &
