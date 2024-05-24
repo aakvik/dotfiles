bda1d638e02dfdf4c25e4fb4f70aa0658ed33048
Aleksander Aakvik <aleksander@aakvik.net>
2024-05-24 18:04:48 +0200

    several changes

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,2 +1,18 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+-- Use the system clipboard
+vim.opt.clipboard = "unnamedplus"
+
+-- Yank to both the buffer and primary clipboard
+vim.api.nvim_create_augroup("YankBoth", { clear = true })
+vim.api.nvim_create_autocmd("TextYankPost", {
+  group = "YankBoth",
+  callback = function()
+    if vim.v.event.operator == "y" then
+      local yanked_text = vim.fn.getreg('"')
+      vim.fn.system("xclip -selection clipboard", yanked_text)
+      vim.fn.system("xclip -selection primary", yanked_text)
+    end
+  end,
+})
