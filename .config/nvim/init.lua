e6c5d0dfcb97f9dc79207d48006eed79e0709aa6
Aleksander Aakvik <aleksander@aakvik.net>
2026-06-15 23:23:13 +0200

    disable arrow keys in lazyvim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -5,5 +5,5 @@ require("config.lazy")
 vim.opt.clipboard = "unnamedplus"
 
 -- Delete without overwriting clipboard
-vim.keymap.set({"n", "v"}, "x", '"_x')
-vim.keymap.set({"n", "v"}, "X", '"_X')
+vim.keymap.set({ "n", "v" }, "x", '"_x')
+vim.keymap.set({ "n", "v" }, "X", '"_X')
--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -2,3 +2,9 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 --
+
+-- Force hjkl — disable arrow keys
+for _, key in ipairs({ "<Up>", "<Down>", "<Left>", "<Right>" }) do
+  vim.keymap.set({ "n", "v" }, key, "<Nop>")
+  vim.keymap.set("i", key, "<Nop>")
+end
